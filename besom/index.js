f2511386e70bbbc7d8268bb9bb34d6f90d75a8fe
yanghb5 <yanghb5@lenovo.com>
2020-04-08 16:08:29 +0800

    ud

--- besom/index.js
+++ besom/index.js
@@ -268,6 +268,8 @@
           center = startInfo.center, mcy = movetouches[0].pageY - center.pageY, mpx = movetouches[0].pageX - preMove.pageX,
           rotate = mcy*mpx > 0 ? -totalrotate : totalrotate;
 
+        document.getElemementById('test').innerHTML = totalrotate + '------' + rotate + '--scale' + scale - 1
+
         if(!name) name = enabled('pinch') && enabled('rotate') ? (Math.abs(scale - 1) > totalrotate/360 ? 'pinch' : 'rotate') : (enabled('pinch') ? 'pinch' : 'rotate');
 
         if(name == 'pinch'){
